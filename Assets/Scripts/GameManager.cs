21a2b3206baad02d6167bd29776b30c963deb998
Ryan Jones <RyanMan56@gmail.com>
2018-12-02 22:40:35 +0000

    progress after ludum dare

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private GameObject blobs;
+    private GameObject selectedBlob;
+    private GameObject arrow;
+    private GameObject arrowSpriteMask;    
+    private bool arrowCharging = false;
+    private float currentCharge = 4.54f;
+    private float chargingSpeed = 5f;
+    private float arrowMinX = 4.54f;
+    private float arrowMaxX = 7.46f;
+    private float chargeAngle;
+    private float maxForce = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        blobs = GameObject.Find("Blobs");        
+        arrow = GameObject.Find("Arrow");
+        arrowSpriteMask = arrow.transform.Find("SpriteMask").gameObject;
+
+        arrow.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetRandomBlob();
+            //arrow.transform.parent = selectedBlob.transform;
+            currentCharge = arrowMinX;            
+            arrowCharging = true;
+        }
+        if (selectedBlob != null)
+        {
+            PositionArrow();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            arrow.SetActive(true);
+            AngleArrow();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            arrow.SetActive(false);
+            arrowCharging = false;
+            CalculateImpulse();
+        }
+        if (arrowCharging)
+        {
+            if (currentCharge < arrowMinX)
+            {
+                chargingSpeed = Mathf.Abs(chargingSpeed);
+            }
+            if (currentCharge > arrowMaxX)
+            {
+                chargingSpeed = -Mathf.Abs(chargingSpeed);
+            }
+            currentCharge += chargingSpeed * Time.deltaTime;
+            arrowSpriteMask.transform.localPosition = new Vector2(currentCharge, arrowSpriteMask.transform.localPosition.y);
+        }
+	}
+
+    private void GetRandomBlob()
+    {
+        selectedBlob = blobs.transform.GetChild(Random.Range(0, blobs.transform.childCount)).gameObject;
+    }
+
+    private void PositionArrow()
+    {
+        arrow.transform.position = selectedBlob.transform.position;
+    }
+
+    private void AngleArrow()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var arrowPos = arrow.transform.position;
+
+        chargeAngle = Mathf.Atan2((mousePos.y - arrowPos.y), (mousePos.x - arrowPos.x)) * Mathf.Rad2Deg;
+        arrow.transform.localRotation = Quaternion.Euler(0, 0, chargeAngle);
+    }
+
+    private void CalculateImpulse()
+    {
+        var strength = (currentCharge - arrowMinX) / (arrowMaxX - arrowMinX);
+        var actualStrength = maxForce * strength;
+
+        var xStrength = actualStrength * Mathf.Cos(chargeAngle * Mathf.Deg2Rad);// * Mathf.Rad2Deg;
+        var yStrength = actualStrength * Mathf.Sin(chargeAngle * Mathf.Deg2Rad);// * Mathf.Rad2Deg;
+
+        Debug.Log("s: " + actualStrength + " angle: " + chargeAngle + " x: " + xStrength + " y: " + yStrength);
+
+        selectedBlob.GetComponent<Rigidbody2D>().AddForce(new Vector2(xStrength, yStrength), ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Guy.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guy : MonoBehaviour {
+
+    public int guyNumber;
+    public bool randomise = true;
+    private GameObject bodyObj;
+    private GameObject eyesObj;
+    // How long the blink lasts
+    private float blinkDuration;
+    // Time between blinks
+    private float blinkPause;
+    private float blinkDurationCountdown = 0;
+    private float blinkPauseCountdown = 0;
+    private enum blinkStates { eyesOpen, eyesClosed }
+    private blinkStates blinkState = blinkStates.eyesOpen;
+
+    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer eyesSpriteRenderer;
+    private Sprite bodySprite;
+    private Sprite eyesSprite;
+
+    private Rigidbody2D rb;
+
+    private int groundCollisions = 0;
+    private bool canJump = false;
+
+	// Use this for initialization
+	void Start () {
+        if (randomise)
+        {
+            guyNumber = Random.Range(1, 38);
+        }
+
+        bodyObj = gameObject;
+        eyesObj = transform.Find("Eyes").gameObject;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        eyesSpriteRenderer = eyesObj.GetComponent<SpriteRenderer>();       
+
+        bodySprite = Resources.Load<Sprite>("Images/" + guyNumber + "_body");     
+        eyesSprite = Resources.Load<Sprite>("Images/" + guyNumber + "_eyes");
+        spriteRenderer.sprite = bodySprite;
+        eyesSpriteRenderer.sprite = eyesSprite;
+
+        bodyObj.AddComponent<PolygonCollider2D>();
+
+        blinkDuration = Random.Range(0.25f, 0.5f);
+        blinkPause = Random.Range(3, 5);
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Blink();
+        LookTowardsCursor();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            groundCollisions++;
+            canJump = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            groundCollisions--;
+            if (groundCollisions <= 0)
+            {
+                canJump = false;
+            }
+        }
+    }
+
+    void Blink()
+    {
+        if (blinkPauseCountdown <= 0 && blinkState == blinkStates.eyesOpen)
+        {
+            blinkState = blinkStates.eyesClosed;
+            eyesSprite = Resources.Load<Sprite>("Images/" + guyNumber + "_eyes_closed");
+            eyesSpriteRenderer.sprite = eyesSprite;
+            blinkDurationCountdown = blinkDuration;
+        }
+        if (blinkDurationCountdown <= 0 && blinkState == blinkStates.eyesClosed)
+        {
+            blinkState = blinkStates.eyesOpen;
+            eyesSprite = Resources.Load<Sprite>("Images/" + guyNumber + "_eyes");
+            eyesSpriteRenderer.sprite = eyesSprite;
+            blinkPauseCountdown = blinkPause;
+        }
+        blinkPauseCountdown -= Time.deltaTime;
+        blinkDurationCountdown -= Time.deltaTime;
+    }
+
+    void LookTowardsCursor()
+    {
+        //var cameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //var eyesPos = eyesObj.transform.position;
+    }
+
+    void Move()
+    {
+        if (Input.GetAxis("Vertical") > 0 && canJump)
+        {
+            rb.AddForce(new Vector2(0, 2), ForceMode2D.Impulse);
+        }
+        if (Input.GetAxis("Horizontal") > 0 && canJump)
+        {
+            rb.AddForce(new Vector2(1, 1), ForceMode2D.Impulse);
+        }
+        if (Input.GetAxis("Horizontal") < 0 && canJump)
+        {
+            rb.AddForce(new Vector2(-1, 1), ForceMode2D.Impulse);
+        }        
+    }
+}
